7a12d8ac1f47396824708dbed5ea39c0e9046060
John Omotani <john.omotani@ukaea.uk>
2022-02-01 15:46:52 +0100

    Update build scripts for intelmpi

--- marconi-gnu-intelmpi/scripts/build-hdf5.sh
+++ marconi-gnu-intelmpi/scripts/build-hdf5.sh
@@ -5,10 +5,16 @@ set -e
 # HDF5
 cd dependencies
 DEPS_ROOT=$(pwd)
-mkdir hdf5-build
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.bz2
-tar xjf hdf5-1.12.1.tar.bz2
-cd hdf5-1.12.1
-./configure --prefix $DEPS_ROOT/hdf5-build --enable-build-mode=production
-make -j 16
-make install
+
+# To configure with CMake, have to download a special tarball, not the standard source-code tarball
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.13/hdf5-1.13.0/src/CMake-hdf5-1.13.0.tar.gz
+tar xzf CMake-hdf5-1.13.0.tar.gz
+
+# Weird build setup, see https://portal.hdfgroup.org/display/support/Building+HDF5+with+CMake
+cd CMake-hdf5-1.13.0/
+./build-unix.sh
+tar xzf HDF5-1.13.0-Linux.tar.gz
+
+# Create a symbolic link to the built HDF5 directory, because HDF5 creates it at an insanely complicated path
+cd $DEPS_ROOT
+ln -s CMake-hdf5-1.13.0/HDF5-1.13.0-Linux/HDF_Group/HDF5/1.13.0/ hdf5-build
--- marconi-gnu-intelmpi/scripts/build-netcdf-cxx4.sh
+++ marconi-gnu-intelmpi/scripts/build-netcdf-cxx4.sh
@@ -6,9 +6,14 @@ set -e
 # Installed to the same directory as netcdf-c, because this makes it easier for BOUT++'s config
 cd dependencies
 DEPS_ROOT=$(pwd)
-wget https://downloads.unidata.ucar.edu/netcdf-cxx/4.3.1/netcdf-cxx4-4.3.1.tar.gz
-tar xzf netcdf-cxx4-4.3.1.tar.gz
-cd netcdf-cxx4-4.3.1
-CPPFLAGS="-I$DEPS_ROOT/hdf5-build/include -I$DEPS_ROOT/netcdf-build/include" LDFLAGS="-L$DEPS_ROOT/hdf5-build/lib/ -L$DEPS_ROOT/netcdf-build/lib/" ./configure --prefix=$DEPS_ROOT/netcdf-build
-make -j 16
-make install
+
+# Use github.com version for now, because CMake bug-fixes have not made it into
+# the released version yet (should be able to use 4.3.2 when that comes out).
+git clone git@github.com:Unidata/netcdf-cxx4.git
+cd netcdf-cxx4
+# Get known-good version (was master branch on 1/2/2022)
+git checkout 6b1ae2666f2480a8ef7db6db56760834d912a9fa
+
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DEPS_ROOT/netcdf-build/ -DCMAKE_FIND_ROOT_PATH=$DEPS_ROOT/hdf5-build/
+cmake --build build -j 16
+cmake --install build
