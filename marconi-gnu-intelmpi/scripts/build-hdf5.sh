e0a2d48c49daeabb6773f78bad4cae6d375d8b98
John Omotani <john.omotani@ukaea.uk>
2022-02-01 17:58:29 +0100

    Go back to hdf5-1.13.0
    CMake configuration for hdf5-1.12.1 doesn't work, don't know why.

--- marconi-gnu-intelmpi/scripts/build-hdf5.sh
+++ marconi-gnu-intelmpi/scripts/build-hdf5.sh
@@ -7,14 +7,14 @@ cd dependencies
 DEPS_ROOT=$(pwd)
 
 # To configure with CMake, have to download a special tarball, not the standard source-code tarball
-wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/CMake-hdf5-1.12.1.tar.gz
-tar xzf CMake-hdf5-1.12.1.tar.gz
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.13/hdf5-1.13.0/src/CMake-hdf5-1.13.0.tar.gz
+tar xzf CMake-hdf5-1.13.0.tar.gz
 
 # Weird build setup, see https://portal.hdfgroup.org/display/support/Building+HDF5+with+CMake
-cd CMake-hdf5-1.12.1/
+cd CMake-hdf5-1.13.0/
 ./build-unix.sh
-tar xzf HDF5-1.12.1-Linux.tar.gz
+tar xzf HDF5-1.13.0-Linux.tar.gz
 
 # Create a symbolic link to the built HDF5 directory, because HDF5 creates it at an insanely complicated path
 cd $DEPS_ROOT
-ln -s --no-target-directory CMake-hdf5-1.12.1/HDF5-1.12.1-Linux/HDF_Group/HDF5/1.12.1/ hdf5-build
+ln -s --no-target-directory CMake-hdf5-1.13.0/HDF5-1.13.0-Linux/HDF_Group/HDF5/1.13.0/ hdf5-build
